6d8e68d9b74dfaf21a20a1216a8762f67b371567
Lee <meetleev@gmail.com>
2022-03-10 11:47:35 +0800

    init

---
+++ gulpfile.js
@@ -0,0 +1,149 @@
+const gulp = require('gulp');
+const Concat = require('gulp-concat');
+const Babel = require("gulp-babel");
+const Wrap = require('gulp-wrap');
+const Replace = require('gulp-replace');
+const Del = require('del');
+const fs = require('fs-extra');
+const ps = require('path');
+const realFs = require('fs');
+const rollup = require("rollup");
+const commonjs = require('@rollup/plugin-commonjs');
+const rpBabel = require('@rollup/plugin-babel');
+const babel_preset_env = require('@babel/preset-env');
+const babel_preset_typescript = require('@babel/preset-typescript');
+const {nodeResolve} = require('@rollup/plugin-node-resolve');
+const babel_plugin_transform_for_of = require('@babel/plugin-transform-for-of');
+const rollup_plugin_terser = require("rollup-plugin-terser");
+
+const json = JSON.parse(fs.readFileSync('./package.json'));
+const SDK_NAME = json.sdkName;
+let dependenciesFiles = [
+    'source/crypto-js/crypto-js-min.js',
+];
+
+function buildSDKCryptoDependencies() {
+    return gulp.src(dependenciesFiles)
+        .pipe(Concat('crypto-js.js'))
+        .pipe(gulp.dest('bin/'))
+}
+
+
+let dependenciesConfigFiles = [
+    './source/SDKConfig.js',
+];
+
+function buildSDKConfigDependencies() {
+    return gulp.src(dependenciesConfigFiles)
+        .pipe(Concat('SDKConfig.js'))
+        .pipe(Babel({
+            presets: ['@babel/preset-env']
+        }))
+        .pipe(Wrap({src: 'source/template.txt'}, {variable: 'data'}))
+        .pipe(Replace(/SDK_NAME/g, SDK_NAME))
+        // .pipe(Uglify({
+        //     mangle: {
+        //         eval: true, toplevel: false, properties: {regex: /^_/},
+        //     },
+        //     compress: {
+        //         unused: true,
+        //         keep_fargs: false,
+        //         unsafe: true
+        //     },
+        //     keep_classnames:true,
+        //     output: {
+        //         beautify: true,
+        //     }
+        //
+        // }))
+        .pipe(gulp.dest('bin/'))
+}
+
+async function buildSDKScripts() {
+    const engineRoot = ps.resolve(__dirname);
+    const realpath = typeof realFs.realpath.native === 'function' ? realFs.realpath.native : realFs.realpath;
+    const realPath = (file) => new Promise((resolve, reject) => {
+        realpath(file, (err, path) => {
+            if (err && err.code !== 'ENOENT') {
+                reject(err);
+            } else {
+                resolve(err ? file : path);
+            }
+        });
+    });
+    const bundle = await rollup.rollup({
+        input: './source/index.ts',
+        external: ['cc'],
+        plugins: [
+            nodeResolve({
+                extensions: ['.ts', '.js'],
+                jail: await realPath(engineRoot),
+                rootDir: engineRoot,
+                modulesOnly: true,
+            }),
+            commonjs({/*extensions: ['.ts'], exclude: ["source/!**!/!*.js"]*/}),
+            rpBabel.babel({
+                extensions: ['.ts', '.js'],
+                babelHelpers: 'bundled',
+                comments: false,
+                // exclude: ['node_modules/!**'],
+                // include: [/*"exports/!**!/!*.ts"*/],
+                plugins: [
+                    [babel_plugin_transform_for_of.default, {assumeArray: true}],
+                ],
+                presets: [
+                    [babel_preset_env.default, {loose: true,}],
+                    [babel_preset_typescript]/*['@cocos/babel-preset-cc']*/]
+            }),
+            rollup_plugin_terser.terser({
+                compress: {
+                    reduce_funcs: false,
+                    keep_fargs: false,
+                    unsafe_Function: true,
+                    unsafe_math: true,
+                    unsafe_methods: true,
+                    passes: 2, // first: remove deadcodes and const objects, second: drop variables
+                },
+                mangle: {
+                    eval: true, toplevel: false, properties: {regex: /^_/},
+                },
+                keep_fnames: false,
+                /*output: {
+                    beautify: false,
+                },*/
+                toplevel: false,
+            }),
+        ]
+    });
+
+    const rollupOutput = await bundle.write({
+        file: `./bin/${SDK_NAME}.js`,
+        format: 'iife',
+        esModule: false,
+        sourcemap: false,
+        name: SDK_NAME,
+        globals: {'cc': 'cc'},
+    });
+    // console.log('rp', rollupOutput)
+}
+
+function clean() {
+    return Del(['./bin/*']);
+}
+
+exports.clean = gulp.series(clean);
+
+async function buildDTS() {
+    const outDir = ps.join('bin'/*, '.declarations'*/);
+    await fs.emptyDir(outDir);
+    return require('gen-dts').generate({
+        rootDir: __dirname,
+        outDir: outDir,
+        rootModuleName: SDK_NAME,
+        nonExportedThirdLibs: ['cc'],
+    });
+}
+
+exports.buildDTS = gulp.series(buildDTS);
+exports.buildSDKSrcs = gulp.series(buildSDKScripts);
+exports.default = exports.build = gulp.series(clean, /*buildSDKCryptoDependencies, buildSDKConfigDependencies,*/ buildDTS, buildSDKScripts);
---
+++ source/index.ts
@@ -0,0 +1,8 @@
+export * from './Log';
+export * from './sdk/EventManager';
+export {resMgr} from './sdk/ResMgr';
+export * from './sdk/AudioMgr';
+export * from './sdk/UIMgr';
+export * from './sdk/ObjectPoolsMgr';
+
+export * from './sdk/utils';
